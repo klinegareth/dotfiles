48742e1a66b3ad09de3962bfdea99f7f03cf0ccc
klinegareth <klinegareth@gmail.com>
2022-01-09 18:06:31 -0500

    Switched back to fish, started porting zsh config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,13 @@
 #Greeting 
-set fish_greeting ""
+theme_gruvbox dark hard
+
+# Spacefish settings
+set SPACEFISH_PROMPT_ADD_NEWLINE false
+set SPACEFISH_VI_MODE_PREFIX ""
+set SPACEFISH_CHAR_SYMBOL "ʕ·ᴥ·ʔ-" 
+set SPACEFISH_CHAR_COLOR_SUCCESS normal
+set SPACEFISH_TIME_SHOW true
+
 
 # Start X at login
 if status is-login
@@ -9,28 +17,35 @@ if status is-login
 end
 
 #Aliases 
-alias dotfiles "/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME" 
-alias xmonadconf "nvim ~/.xmonad/xmonad.hs"
-alias xinitconf "nvim ~/.xinitrc"
-alias conkyconf "nvim ~/.conkyrc"
-alias userchrome "nvim ~/.mozilla/firefox/4kfdya0u.default-release/chrome/userChrome.css"
-alias fishconf 'nvim ~/.config/fish/config.fish -c "set syntax=bash"'
-alias conkylconf "nvim ~/.config/conky/conkyL.conf"
-alias conkyrconf "nvim ~/.config/conky/conkyR.conf"
-alias i3conf "nvim ~/.config/i3/config"
-alias gnotes "nvim ~/.notes.txt -c 'Go 60%x50%'"
-alias todo 'nvim ~/.todo.txt -c "setlocal foldmethod=indent" -c "Go 60%x50% | highlight Normal ctermbg=NONE guibg=NONE | highlight LineNr ctermbg=NONE guibg=NONE | highlight SignColumn ctermbg=NONE guibg=NONE"'
+alias dotfiles="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME" 
+alias dcommit='/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME commit -am' 
+alias dstatus="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME status" 
+alias dpush="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME push" 
+alias dpull="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME pull" 
+alias xmonadconf="nvim ~/.xmonad/xmonad.hs"
+alias xinitconf="nvim ~/.xinitrc"
+alias alacrittyconf="nvim ~/.config/alacritty/alacritty.yml"
+alias conkyconf="nvim ~/.conkyrc"
+alias userchrome="nvim ~/.mozilla/firefox/*.default-release/chrome/userChrome.css"
+alias fishconf='nvim ~/.config/fish/config.fish -c "set syntax=bash"'
+alias zshconf='nvim ~/.zshrc'
+alias conkylconf="nvim ~/.config/conky/conkyL.conf"
+alias conkyrconf="nvim ~/.config/conky/conkyR.conf"
+alias i3conf="nvim ~/.config/i3/config"
+alias gnotes="nvim ~/.notes.txt -c 'Go 60%x50%'"
+alias todo='nvim ~/.todo.txt -c "setlocal foldmethod=indent" -c "Go 60%x50% | highlight Normal ctermbg=NONE guibg=NONE | highlight LineNr ctermbg=NONE guibg=NONE | highlight SignColumn ctermbg=NONE guibg=NONE"'
+alias n="nvim"
 
-#Prompt
-function fish_prompt --description 'custom fish prompt'
-  set_color ffecdd
-  printf (pwd)
-    echo -e ''
-  echo "ʕ·ᴥ·ʔ- "
-end
-
-function fish_right_prompt --description 'custom fish right prompt'
-  set_color ffecdd
-    date '+[%H:%M]'
-end
+##Prompt
+#function fish_prompt --description 'custom fish prompt'
+#  set_color ffecdd
+#  printf (pwd)
+#    echo -e ''
+#  echo "ʕ·ᴥ·ʔ- "
+#end
+#
+#function fish_right_prompt --description 'custom fish right prompt'
+#  set_color ffecdd
+#    date '+[%H:%M]'
+#end
 
